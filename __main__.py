1b56a61360dab895ad61ac57c076a467f2698624
Maxence Cossette <maxencek7@gmail.com>
2022-03-28 22:47:47 -0400

    c vrm fucking fort

--- __main__.py
+++ __main__.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
 import sys
+import threading
 import time
 from networktables import NetworkTables
+from playsound import playsound
 
 import logging
 
@@ -13,9 +14,30 @@ if len(sys.argv) != 2:
 
 ip = sys.argv[1]
 
+cond = threading.Condition()
+notified = [False]
+
+def connectionListener(connected, info):
+    print(info, '; Connected=%s' % connected)
+    with cond:
+        notified[0] = True
+        cond.notify()
+
 NetworkTables.initialize(server=ip)
 
 sd = NetworkTables.getTable("SmartDashboard")
+NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
+
+playsound('sons/jingle.mp3',block=False)
+
+with cond:
+    playsound('sons/attente_conn.mp3')
+    print("Waiting")
+    if not notified[0]:
+        cond.wait()
+
+print("Connectee")
+playsound('sons/conn.mp3')
 
 i = 0
 while True:
