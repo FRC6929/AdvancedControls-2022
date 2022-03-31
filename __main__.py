fdee5a204ca8a646bb0b220fcb7b91d54a04ae86
davido08 <maxencek7@gmail.com>
2022-03-31 12:40:42 -0400

    serial

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 import sys
 import threading
 import time
+import serial
 from networktables import NetworkTables
 from playsound import playsound
 
@@ -13,6 +14,8 @@ if len(sys.argv) != 2:
     exit(0)
 
 ip = sys.argv[1]
+
+ser = serial.Serial(sys.argv[2], 9600, 1)
 
 cond = threading.Condition()
 notified = [False]
@@ -28,12 +31,25 @@ def connectionListener(connected, info):
         pass
     pass
 
+def clear_left(x):
+    global ser
+    ser.write('A')
+    ser.write('B')
+    ser.write('C')
+    ser.write('D')
+    ser.write('E')
+    ser.write('F')
+
 def onValueSwitch(source, key, value, isNew):
     global palert
     if key == 'Mode':
         if value == 'Shooter':
+            ser.write('h')
+            ser.write('I')
             playsound('sons/shooter.mp3')
         elif value == 'Elevateur':
+            ser.write('H')
+            ser.write('i')
             playsound('sons/elevator.mp3')
     elif key == 'AlertPression':
         if value == True:
@@ -42,28 +58,37 @@ def onValueSwitch(source, key, value, isNew):
             last_palert = time.time()
         else:
             palert = False
-    elif key == 'Bras':
-        if value == 'fixe':
-            playsound('sons/bras_fixe.mp3')
-        elif value == 'mobile':
-            playsound('sons/bras_mobile.mp3')
     elif key == 'shooter_Set':
         if value == 'lvl1_av':
+            clear_left()
+            ser.write('a')
             playsound('sons/lvl1_av.mp3')
             pass
         elif value == 'lvl1_arr':
+            clear_left()
+            ser.write('b')
             playsound('sons/lvl1_arr.mp3')
             pass
         elif value == 'lvl2_av':
+            clear_left()
+            ser.write('c')
             playsound('sons/lvl2_av.mp3')
             pass
         elif value == 'lvl2_arr':
+            clear_left()
+            ser.write('d')
             playsound('sons/lvl2_arr.mp3')
             pass
         elif value == 'intake':
+            clear_left()
+            ser.write('e')
             playsound('sons/intake.mp3')
             pass
-        
+        elif key == 'Bras':
+            if value == 'fixe':
+                playsound('sons/bras_fixe.mp3')
+            elif value == 'mobile':
+                playsound('sons/bras_mobile.mp3')
 
 NetworkTables.initialize(server=ip)
 
@@ -91,3 +116,5 @@ while True:
         playsound('sons/palert.mp3',block=True)
         pass
     time.sleep(1)
+
+ser.close()
